d09961d8c546400e5d0e153b7a3b24deded742a6
Alexandra Peres <alexandra.peres@hotmail.com>
2021-04-17 16:26:32 -0300

    Desenvolver endpoint de autenticação do adm

--- src/main/java/com/modelagem/software/jogo/mario/world/jogo/service/AdministradorService.java
+++ src/main/java/com/modelagem/software/jogo/mario/world/jogo/service/AdministradorService.java
@@ -2,11 +2,12 @@ package com.modelagem.software.jogo.mario.world.jogo.service;
 
 import com.modelagem.software.jogo.mario.world.jogo.entity.Administrador;
 import com.modelagem.software.jogo.mario.world.jogo.repository.AdministradorRepository;
+import com.modelagem.software.jogo.mario.world.jogo.retorno.DtoRetorno;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @Getter
@@ -15,8 +16,18 @@ public class AdministradorService {
     @Autowired
     private AdministradorRepository repository;
 
-    public List<Administrador> retornarDaBaseTeste(){
-        return repository.findAll();
+
+    public DtoRetorno autenticar(String username, String password){
+
+        Optional<Administrador> adm = repository.findByUsernameAndPassword(username,
+                                                                           password);
+
+        if(adm.isPresent()){
+            return DtoRetorno.builder().sucesso(true).build();
+        }
+
+        return DtoRetorno.builder().sucesso(false).mensagem("Senha ou usuário incorretos.").build();
+
     }
 
 }
