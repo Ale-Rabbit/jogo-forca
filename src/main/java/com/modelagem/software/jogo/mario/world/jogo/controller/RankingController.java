b6249cc9c0f2d36c29c5a633cb7b6c46777f95b6
Alexandra Peres <alexandra.peres@hotmail.com>
2021-04-29 21:47:42 -0300

    Criar endpoint para somente inserir na tabela ranking

--- src/main/java/com/modelagem/software/jogo/mario/world/jogo/controller/RankingController.java
+++ src/main/java/com/modelagem/software/jogo/mario/world/jogo/controller/RankingController.java
@@ -2,20 +2,27 @@ package com.modelagem.software.jogo.mario.world.jogo.controller;
 
 import java.util.List;
 
+import com.modelagem.software.jogo.mario.world.jogo.retorno.DtoRetorno;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.modelagem.software.jogo.mario.world.jogo.entity.Ranking;
 import com.modelagem.software.jogo.mario.world.jogo.service.RankingService;
 
 @RestController
+@RequestMapping(value = "/ranking")
 public class RankingController {
 
     @Autowired
     private RankingService service;
 
-    @GetMapping("/ranking")
+    @PostMapping(value = "/{username}/{pontuacao}")
+    public DtoRetorno salvarRanking(@PathVariable String username,
+                                    @PathVariable Integer pontuacao){
+        return service.salvarRanking(username, pontuacao);
+    }
+
+    @GetMapping
     public List<Ranking> findAllRanking(){
         return service.getRanking();
     }
--- src/main/java/com/modelagem/software/jogo/mario/world/jogo/service/RankingService.java
+++ src/main/java/com/modelagem/software/jogo/mario/world/jogo/service/RankingService.java
@@ -1,7 +1,9 @@
 package com.modelagem.software.jogo.mario.world.jogo.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.modelagem.software.jogo.mario.world.jogo.retorno.DtoRetorno;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,30 @@ public class RankingService {
 
     @Autowired
     private RankingRepository repository;
+
+    public DtoRetorno salvarRanking(String username, Integer pontuacao){
+
+        Optional<Ranking> jogadorExistente = repository.findByNomeJogador(username);
+
+        if (!jogadorExistente.isPresent()){
+
+            Ranking novoJogador = Ranking.builder()
+                                         .nomeJogador(username)
+                                         .pontuacao(pontuacao)
+                                         .build();
+
+            repository.save(novoJogador);
+
+        } else {
+
+            jogadorExistente.get().setPontuacao(pontuacao);
+            repository.save(jogadorExistente.get());
+
+        }
+
+        return DtoRetorno.builder().sucesso(true).build();
+
+    }
 
     public List<Ranking> getRanking(){
         return repository.findAll();
