973ac4f59937ffa0d79ae36bb1fbacbdae792ee4
pedrinnpadilha <pedro.padilha@rede.ulbra.br>
2021-05-07 20:13:56 -0300

    tratamento de excessoes crud adm

---
+++ src/main/java/com/modelagem/software/jogo/mario/world/jogo/retorno/RetornoErroAdm.java
@@ -0,0 +1,10 @@
+package com.modelagem.software.jogo.mario.world.jogo.retorno;
+
+public class RetornoErroAdm {
+	public String message;
+
+    public RetornoErroAdm(String message) {
+        this.message = message;
+    }
+
+}
